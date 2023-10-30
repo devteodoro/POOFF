c60073c2e762a79b96efe0c94d71520eb2926f1f
Filipe Teodoro <filipeteodoro99@gmail.com>
2023-10-29 21:06:44 -0300

    Inicio da responsividade do menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,3 +3,8 @@ window.addEventListener("scroll", () => {
     let header = document.querySelector('#header');
     header.classList.toggle('rolagem', window.scrollY > 300);
 });
+
+document.querySelector('#menu-btn').onclick = () => {
+    document.querySelector('#menu-btn').classList.toggle('fa-times');
+    document.querySelector('.navbar').classList.toggle('active');
+}
