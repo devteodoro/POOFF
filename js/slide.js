b3b949dc350c3f4c94bbdcb54172d99e7700d555
Filipe Teodoro <filipeteodoro99@gmail.com>
2023-11-25 01:55:23 -0300

    adição da section contato, rodape e  resposividade

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,29 @@
-const swiper = new Swiper('.swiper', {
+
+var numberCard = 4;
+
+validaLargura(window.innerWidth, false);
+
+window.addEventListener('resize', (e) => {
+  validaLargura(window.innerWidth, true);    
+}, true);
+
+function validaLargura(largura, resize) {
+    let width = window.innerWidth;
+
+    if(width <= 1200)
+      numberCard = 3;
+
+    if(width <= 768)
+      numberCard = 2;
+
+    if(width <= 520)
+      numberCard = 1;
+
+    return criarSlide(numberCard);
+}
+
+function criarSlide () {
+  const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
@@ -6,7 +31,8 @@ const swiper = new Swiper('.swiper', {
     navigation: true, 
     spaceBetween: 30,
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: numberCard,
+    responsive:true,
 
     autoplay: {
       delay: 5000,
@@ -27,3 +53,17 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+}
+
+function readCard (el) {
+  var card = el.parentElement.parentElement;
+  card.classList.toggle('active');
+
+  if(card.classList.contains('active')) {
+    el.innerText = "Read less";
+  }else{
+    el.innerText = "Read more";
+  }
+  var title = card.getElementsByClassName('title-card')[0].innerText;
+  var text = card.getElementsByClassName('text-card')[0].innerText;          
+}
