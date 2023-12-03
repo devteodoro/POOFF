b027972afcba44dd9e460f41711d809a9314f31c
Filipe Teodoro <filipeteodoro99@gmail.com>
2023-12-02 23:32:32 -0300

    Alteraçõoes nos texto do site

--- js/script.js
+++ js/script.js
@@ -37,6 +37,45 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+// Máscara telefone 
+var celular = document.getElementById('telephone');
 
+celular.addEventListener("input", () => {
 
+    // Remover os caracteres não numéricos usando a expressão regular /\D/g e limitar a 11 dígitos.
+    var limparValor = celular.value.replace(/\D/g, "").substring(0, 11);
+
+    // Dividir a string em um array de caracteres individuais.
+    var numerosArray = limparValor.split("");
+
+    // Criar a variável para receber o número formatado
+    var numeroFormatado = "";
+
+    // Acessa o IF quando a quantidade de números é maior do que zero
+    if (numerosArray.length > 0) {
+        // Formatar o DD e concatenar o valor
+        // slice - extraie uma parte do array
+        // join - unir os elementos do array em uma única string
+        numeroFormatado += `(${numerosArray.slice(0, 2).join("")})`;
+    }
+
+    // Acessa o IF quando a quantidade de números é maior do que dois
+    if (numerosArray.length > 2) {
+        // Formatar o número do telefone e concatenar o valor
+        // slice - extraie uma parte do array
+        // join - unir os elementos do array em uma única string
+        numeroFormatado += ` ${numerosArray.slice(2, 7).join("")}`;
+    }
+
+    // Acessa o IF quando a quantidade de números é maior do que sete
+    if (numerosArray.length > 7) {
+        // Formatar o número do telefone e concatenar o valor
+        // slice - extraie uma parte do array
+        // join - unir os elementos do array em uma única string
+        numeroFormatado += `-${numerosArray.slice(7, 11).join("")}`;
+    }
+
+    // Enviar para o campo o número formatado
+    celular.value = numeroFormatado;
+});
 
