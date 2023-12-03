78b67426945911b055e6cefa17083a33797098e6
Filipe Teodoro <filipeteodoro99@gmail.com>
2023-12-03 13:02:08 -0300

    atualização swiper

--- js/slide.js
+++ js/slide.js
@@ -1,71 +1,60 @@
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  pagination: true,
+  navigation: true,
+  spaceBetween: 30,
+  loop: true,
+  slidesPerView: 'auto',
+  responsive: true,
 
-var numberCard = 4;
-var swiper;
-
-validaLargura(false);
-
-window.addEventListener('resize', (e) => {
-  validaLargura(true);    
-}, true);
-
-function validaLargura(resize) {
-    let width = window.innerWidth;
-
-    if(width <= 1200)
-      numberCard = 3;
-
-    if(width <= 768)
-      numberCard = 2;
-
-    if(width <= 520)
-      numberCard = 1;
-
-    return criarSlide(resize);
-}
-
-function criarSlide (resize) {
-
-  swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    pagination: true, 
-    navigation: true, 
-    spaceBetween: 30,
-    loop: true,
-    slidesPerView: numberCard,
-    responsive:true,
-
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  breakpoints: {
+    1500: {
+      slidesPerView: 4,
     },
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    1200: {
+      slidesPerView: 3,
     },
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 2,
     },
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+    520: {
+      slidesPerView: 1,
     },
-  });
-}
+  },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
-function readCard (el) {
+
+function readCard(el) {
   var card = el.parentElement.parentElement;
   card.classList.toggle('active');
 
-  if(card.classList.contains('active')) {
-    el.innerText = "Read less";
-  }else{
-    el.innerText = "Read more";
+  if (card.classList.contains('active')) {
+    // swiper.autoplay.stop();
+    el.innerText = "Leia menos";
+  } else {
+    // swiper.autoplay.start()
+    el.innerText = "Leia mais";
   }
-  var title = card.getElementsByClassName('title-card')[0].innerText;
-  var text = card.getElementsByClassName('text-card')[0].innerText;          
+  // var title = card.getElementsByClassName('title-card')[0].innerText;
+  // var text = card.getElementsByClassName('text-card')[0].innerText;
 }
